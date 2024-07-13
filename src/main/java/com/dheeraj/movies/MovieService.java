3d4aa48b6ee733df84c5f7be2bc248805e0599b2
DheerajVakkala <dheerajvakkala98@gmail.com>
2024-07-12 20:46:50 -0400

    removed unecessary code:

--- src/main/java/com/dheeraj/movies/MovieService.java
+++ src/main/java/com/dheeraj/movies/MovieService.java
@@ -15,7 +15,7 @@ public class MovieService {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
-    
+
     public Optional<Movie> singleMovie(String imdbId) {
         return movieRepository.findMovieByImdbId(imdbId);
     }
