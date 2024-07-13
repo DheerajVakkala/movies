2d0c1562ae8586a55a4a0c7c4b5020a97ef8395f
DheerajVakkala <dheerajvakkala98@gmail.com>
2024-07-12 20:32:09 -0400

    added ability to add reviews in the movies

--- src/main/java/com/dheeraj/movies/MovieController.java
+++ src/main/java/com/dheeraj/movies/MovieController.java
@@ -1,6 +1,5 @@
 package com.dheeraj.movies;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -23,8 +23,13 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Movie> getSingleMovie(@PathVariable ObjectId id) {
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+//        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+//    }
 
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
     }
 }
